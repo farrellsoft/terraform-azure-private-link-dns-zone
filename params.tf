b25f77909322e4362725dd72c1601f9d5b5e3918
Jason Farrell <imjason@gmail.com>
2023-02-20 22:48:38 -0500

    initial commit:

---
+++ params.tf
@@ -0,0 +1,10 @@
+
+variable domain_name {
+  type        = "string"
+  description = "The domain name to use for DNS routing"
+}
+
+variable resource_group_name {
+  type        = "string"
+  description = "The name of the resource group in which to create the resources"
+}
