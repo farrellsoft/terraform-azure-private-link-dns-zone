13e3e1dd410f752d6e77b92d8e2753607e8cda47
Jason Farrell <imjason@gmail.com>
2023-03-01 22:09:54 -0500

    added support for the function apps dns zone

--- locals.tf
+++ locals.tf
@@ -1,14 +1,15 @@
 locals {
   zone_mapping = {
-    "sql"       = "privatelink.database.windows.net"
-    "blob"      = "privatelink.blob.core.windows.net"
-    "queue"     = "privatelink.queue.core.windows.net"
-    "file"      = "privatelink.file.core.windows.net"
-    "table"     = "privatelink.table.core.windows.net"
-    "cosmossql" = "privatelink.documents.azure.com"
-    "vault"     = "privatelink.vaultcore.azure.net"
-    "eventhub"  = "privatelink.servicebus.windows.net"
-    "eventgrid" = "privatelink.eventgrid.azure.net"
-    "redis"     = "privatelink.redis.cache.windows.net"
+    "sql"         = "privatelink.database.windows.net"
+    "blob"        = "privatelink.blob.core.windows.net"
+    "queue"       = "privatelink.queue.core.windows.net"
+    "file"        = "privatelink.file.core.windows.net"
+    "table"       = "privatelink.table.core.windows.net"
+    "cosmossql"   = "privatelink.documents.azure.com"
+    "vault"       = "privatelink.vaultcore.azure.net"
+    "eventhub"    = "privatelink.servicebus.windows.net"
+    "eventgrid"   = "privatelink.eventgrid.azure.net"
+    "redis"       = "privatelink.redis.cache.windows.net"
+    "functionapp" = "privatelink.azurewebsites.net"
   }
 }
