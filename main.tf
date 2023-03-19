dd26f81000a82d272ec677306df98a651cd30fa7
Jason Farrell <imjason@gmail.com>
2023-03-19 15:20:57 -0400

    converted to azapi

--- main.tf
+++ main.tf
@@ -1,12 +1,49 @@
 
-resource azurerm_private_dns_zone this {
-  name                = local.zone_mapping[lower(var.resource_type)]
-  resource_group_name = var.resource_group_name
+#resource azurerm_private_dns_zone this {
+#  name                = local.zone_mapping[lower(var.resource_type)]
+#  resource_group_name = var.resource_group_name
+#}
+
+#resource azurerm_private_dns_zone_virtual_network_link this {
+#  name                  = "link-${lower(var.resource_type)}"
+#  resource_group_name   = var.resource_group_name
+#  private_dns_zone_name = azurerm_private_dns_zone.this.name
+#  virtual_network_id    = var.virtual_network_id
+#}
+
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
 }
 
-resource azurerm_private_dns_zone_virtual_network_link this {
-  name                  = "link-${lower(var.resource_type)}"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.this.name
-  virtual_network_id    = var.virtual_network_id
+data azurerm_client_config current {}
+
+locals {
+  resource_group_id     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
+}
+
+resource azapi_resource this {
+  type              = "Microsoft.Network/privateDnsZones@2020-06-01"
+  name              = local.zone_mapping[lower(var.resource_type)]
+  parent_id         = local.resource_group_id
+  location          = "global"
+}
+
+resource azapi_resource this_link {
+  type              = "Microsoft.Network/privateDnsZones/virtualNetworkLinks@2020-06-01"
+  name              = "link-${lower(var.resource_type)}"
+  parent_id         = azapi_resource.this.id
+  location          = "global"
+
+  body          = jsonencode({
+    properties        = {
+      virtualNetwork = {
+        id = var.virtual_network_id
+      }
+      registrationEnabled = false
+    }
+  })
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output id {
-  value           = azurerm_private_dns_zone.this.id
+  value           = azapi_resource.this.id
   description     = "The ID of the Private DNS Zone."
 }
-
-output name {
-  value           = azurerm_private_dns_zone.this.name
-  description     = "The Name of the Private DNS Zone."
-}
+#
+#output name {
+#  value           = azurerm_private_dns_zone.this.name
+#  description     = "The Name of the Private DNS Zone."
+#}
