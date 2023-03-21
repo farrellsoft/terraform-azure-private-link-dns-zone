63edf3c579438f7a2f11342180becf92c1f936c3
Jason Farrell <imjason@gmail.com>
2023-03-20 22:09:52 -0400

    added version requirements

--- main.tf
+++ main.tf
@@ -1,20 +1,9 @@
 
-#resource azurerm_private_dns_zone this {
-#  name                = local.zone_mapping[lower(var.resource_type)]
-#  resource_group_name = var.resource_group_name
-#}
-
-#resource azurerm_private_dns_zone_virtual_network_link this {
-#  name                  = "link-${lower(var.resource_type)}"
-#  resource_group_name   = var.resource_group_name
-#  private_dns_zone_name = azurerm_private_dns_zone.this.name
-#  virtual_network_id    = var.virtual_network_id
-#}
-
 terraform {
   required_providers {
     azapi = {
-      source = "Azure/azapi"
+      source  = "Azure/azapi"
+      version = ">=1.4.0"
     }
   }
 }
